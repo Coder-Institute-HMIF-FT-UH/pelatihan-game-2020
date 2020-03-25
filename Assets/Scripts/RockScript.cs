4085a0846a0caff65f00cb416170fc8e2f236d39
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-03-25 16:50:28 +0800

    Access Components_Modul 7_Akses Component dalam Script

--- Assets/Scripts/RockScript.cs
+++ Assets/Scripts/RockScript.cs
@@ -2,11 +2,14 @@
 
 public class RockScript : MonoBehaviour
 {
+    // Deklarasi variabel
+    private BoxCollider rockCollider;
+    
     // Start is called before the first frame update
     void Start()
     {
-        // Change rock's position
-        transform.position = new Vector3(1,1,1);
+        rockCollider = GetComponent<BoxCollider>(); // Get component
+        rockCollider.size = new Vector3(5,5,5); // Ubah size collider 
     }
 
     // Update is called once per frame
