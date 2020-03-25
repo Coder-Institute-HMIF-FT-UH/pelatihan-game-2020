71d5f497b1a4d1e199d56185ae255dc619c83a99
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-03-25 16:46:27 +0800

    Access Rock object part 2_Modul 7_Akses GameObject dalam Script

--- Assets/Scripts/RockScript.cs
+++ Assets/Scripts/RockScript.cs
@@ -5,7 +5,8 @@ public class RockScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameObject.Find("Rock").transform.position = new Vector3(1,1,1);
+        // Change rock's position
+        transform.position = new Vector3(1,1,1);
     }
 
     // Update is called once per frame
